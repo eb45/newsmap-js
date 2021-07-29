2ca236aabd6d732d9d9365353d6e22b0f0196015
eb45 <32601823+eb45@users.noreply.github.com>
2021-07-29 17:40:43 -0400

    Update colours.js

--- src/colours.js
+++ src/colours.js
@@ -9,26 +9,4 @@ export const originalColours = {
   "SoC":        "#6b3ea8",
 };
 
-export const primaryColours = {
-  "world":          "#FF0000",
-  "nation":         "#FFFF00",
-  "business":       "#40FF00",
-  "fiveg":     "#00BFFF",
-  "sports":         "#FF00FF",
-  "entertainment":  "#0000FF",
-  "health":         "#FF0080",
-  "science":        "#D8D8D8",
-};
-
-export const rainbow = {
-  "world":          "#FF0000",
-  "nation":         "#FF8000",
-  "business":       "#FFFF00",
-  "fiveg":     "#00FF00",
-  "entertainment":  "#00FF80",
-  "sports":         "#0000FF",
-  "science":        "#800080",
-  "health":         "#808080",
-};
-
 export default originalColours;
