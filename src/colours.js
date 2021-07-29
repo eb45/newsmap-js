ad9bc2eb6d58ea378cfd3a67affb684c8b59aa09
eb45 <32601823+eb45@users.noreply.github.com>
2021-07-29 17:42:06 -0400

    Update colours.js

--- src/colours.js
+++ src/colours.js
@@ -1,12 +1,12 @@
 export const originalColours = {
-  "IoT":          "#ffc7c7",
+  "world":          "#ffc7c7",
   "nation":         "#ffea78",
   "business":       "#b0ff8f",
   "fiveg":     "#04db7e",
   "sports":         "#80caff",
   "entertainment":  "#bfa6ff",
-  "drones":         "#ffadf4",
-  "SoC":        "#6b3ea8",
+  "health":         "#ffadf4",
+  "science":        "#6b3ea8",
 };
 
 export default originalColours;
