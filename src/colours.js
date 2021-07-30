b74065413a621e37cc8fca05365db15f54986fa5
eb45 <32601823+eb45@users.noreply.github.com>
2021-07-30 08:01:04 -0400

    Update colours.js

--- src/colours.js
+++ src/colours.js
@@ -2,7 +2,8 @@ export const originalColours = {
   "iot":          "#ffc7c7",
   "fifth-generation":     "#04db7e",
   "system-on-chip":         "#ffadf4",
-  "drones":        "#6b3ea8"
+  "drones":        "#6b3ea8",
+  "secure-iaas":    "#ADD8E6"
 };
 
 export default originalColours;
